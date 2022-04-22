c412c9766f123b92fb57987c37a538fe25953a96
Wpaswan <dablumcapaswan@gmail.com>
2022-04-21 21:36:52 -0700

    Initial commit

---
+++ DifferenceBetweenTwoDates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DifferenceBetweenTwoDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var prevDate = new DateTime(2022, 1, 1);
+            var today = DateTime.Now;
+            var differenceBetweenDates=today - prevDate;
+            System.Console.WriteLine(prevDate);
+            System.Console.WriteLine(today);
+            System.Console.WriteLine(differenceBetweenDates);
+        }
+    }
+}
